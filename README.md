08dcb054af6b0afa8cfce00ec3d79288a311290c
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-04 15:25:01 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# zsh-fnm
+A simple zsh plugin for the fast and simple Node.js version manager (`fnm`)
+
+## Install
+Using the [:zap: Zap](https://www.zapzsh.org/) minimal zsh plugin manager, add the following to your `.zshrc`
+
+```sh
+# Install plugin
+plug "wintermi/zsh-fnm"
+```
+
+## Prerequisite Checks
+A simple check will be performed to verify that `fnm` has been installed:
+
+- [`fnm`](https://fnm.vercel.app/) - :rocket: Fast Node Manager
+
+
+## License
+The **zsh-fnm** plugin is released under the [Apache License 2.0](https://github.com/wintermi/zsh-fnm/blob/main/LICENSE) unless explicitly mentioned in the file header.
---
+++ zsh-fnm.plugin.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+# shellcheck disable=SC1090
+
+# Exit if the 'fnm' command can not be found
+if ! (( $+commands[fnm] )); then
+    echo "ERROR: 'fnm' command not found"
+    return
+fi
+
+# Add 'fnm' environment variables for 'zsh'
+# Add hook to change Node version on change directory
+eval "$(fnm env --shell zsh --use-on-cd)"
+
+# Completions directory for `fnm` command
+COMPLETIONS_DIR="${0:A:h}/completions"
+
+# Only regenerate completions if older than 24 hours, or does not exist
+if [[ ! -f "$COMPLETIONS_DIR/_fnm"  ||  ! $(find "$COMPLETIONS_DIR/_fnm" -newermt "24 hours ago" -print) ]]; then
+    fnm completions --shell zsh >| "$COMPLETIONS_DIR/_fnm"
+fi
+
+# Add completions to the FPATH
+typeset -TUx FPATH fpath
+fpath=("$COMPLETIONS_DIR" $fpath)
