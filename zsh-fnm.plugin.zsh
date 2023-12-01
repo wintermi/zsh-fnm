4c56ceee560263a3621b381fe9c1912069835bcf
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-12-01 23:09:23 +1100

    feat: change completions copy from 24 hours to 7 days

--- zsh-fnm.plugin.zsh
+++ zsh-fnm.plugin.zsh
@@ -14,7 +14,7 @@ eval "$(fnm env --shell zsh --use-on-cd)"
 # Completions directory for `fnm` command
 local COMPLETIONS_DIR="${0:A:h}/completions"
 
-# Only regenerate completions if older than 24 hours, or does not exist
+# Only regenerate completions if older than 7 days, or does not exist
 if [[ ! -f "$COMPLETIONS_DIR/_fnm"  ||  ! $(find "$COMPLETIONS_DIR/_fnm" -newermt "7 days ago" -print) ]]; then
     fnm completions --shell zsh >| "$COMPLETIONS_DIR/_fnm"
 fi
