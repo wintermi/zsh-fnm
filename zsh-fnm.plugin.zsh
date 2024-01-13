a894ee8e075c7133f09f96667563402d538085ea
Matthew Winter <33818+wintermi@users.noreply.github.com>
2024-01-13 19:10:57 +1100

    feat: modify the way the fnm completions are managed

--- zsh-fnm.plugin.zsh
+++ zsh-fnm.plugin.zsh
@@ -14,11 +14,17 @@ eval "$(fnm env --shell zsh --use-on-cd)"
 # Completions directory for `fnm` command
 local COMPLETIONS_DIR="${0:A:h}/completions"
 
-# Only regenerate completions if older than 7 days, or does not exist
-if [[ ! -f "$COMPLETIONS_DIR/_fnm"  ||  ! $(find "$COMPLETIONS_DIR/_fnm" -newermt "7 days ago" -print) ]]; then
-    fnm completions --shell zsh >| "$COMPLETIONS_DIR/_fnm"
-fi
-
 # Add completions to the FPATH
 typeset -TUx FPATH fpath
 fpath=("$COMPLETIONS_DIR" $fpath)
+
+# If the completion file does not exist yet, then we need to autoload
+# and bind it to `fnm`. Otherwise, compinit will have already done that.
+if [[ ! -f "$COMPLETIONS_DIR/_fnm" ]]; then
+    typeset -g -A _comps
+    autoload -Uz _fnm
+    _comps[fnm]=_fnm
+fi
+
+# Generate and load completion in the background
+fnm completions --shell zsh >| "$COMPLETIONS_DIR/_fnm" &|
